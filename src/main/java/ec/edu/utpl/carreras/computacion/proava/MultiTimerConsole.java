ac124cb3ff30304cc632e1d14640478d9d1a7905
Stalinn99 <s.lennin@yahoo.com>
2026-04-23 09:27:07 -0500

    Parte_4: solucion-join-thread.sleep

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -9,10 +9,20 @@ public class MultiTimerConsole {
         TimerTask timerB = new TimerTask("Temporizador B", 5);
         TimerTask timerC = new TimerTask("Temporizador C", 3);
 
-        new Thread(timerA::run, "Hilo-A").start();
-        new Thread(() -> timerB.run(), "Hilo-B").start();
-        new Thread(() -> timerC.run(), "Hilo-C").start();
+        Thread hiloA = new Thread(() -> timerA.run(), "Hilo-A");
+        Thread hiloB = new Thread(() -> timerB.run(), "Hilo-B");
+        Thread hiloC = new Thread(() -> timerC.run(), "Hilo-C");
 
+        hiloA.start();
+        hiloB.start();
+        hiloC.start();
+        try {
+            hiloA.join();   // espera a que Hilo-A termine
+            hiloB.join();   // espera a que Hilo-B termine
+            hiloC.join();   // espera a que Hilo-C termine
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println(">>> Todos los temporizadores han finalizado.");
     }
-
 }
